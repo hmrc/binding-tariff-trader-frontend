94e3f6fb1e2c641bd3df768023884f38f72e6077
Stefano Sgorlon <gokyo@users.noreply.github.com>
2018-11-29 11:49:41 +0000

    BAU: removing dependency causing deployment issue in MDTP

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,7 +9,6 @@ object AppDependencies {
     "uk.gov.hmrc" %% "bootstrap-play-25"                % "4.0.0",
     "uk.gov.hmrc" %% "govuk-template"                   % "5.25.0-play-25",
     "uk.gov.hmrc" %% "http-caching-client"              % "8.0.0",
-    "uk.gov.hmrc" %% "logback-json-logger"              % "4.1.0",
     "uk.gov.hmrc" %% "play-conditional-form-mapping"    % "0.2.0",
     "uk.gov.hmrc" %% "play-health"                      % "3.8.0-play-25",
     "uk.gov.hmrc" %% "play-language"                    % "3.4.0",
