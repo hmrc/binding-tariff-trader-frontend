7db7ea74709b9640356467cce175e5c812bdb3e5
Ed Wilson <ed.wilson2@hotmail.co.uk>
2018-11-30 11:29:10 +0000

    DIT-514 Service Test

---
+++ test/service/FileServiceSpec.scala
@@ -0,0 +1,32 @@
+package service
+
+import connectors.BindingTariffFilestoreConnector
+import models.response.UploadFileResponse
+import org.mockito.ArgumentMatchers._
+import org.mockito.BDDMockito.given
+import org.scalatest.mockito.MockitoSugar
+import play.api.libs.Files.TemporaryFile
+import play.api.mvc.MultipartFormData
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.play.test.UnitSpec
+
+import scala.concurrent.Future
+
+class FileServiceSpec extends UnitSpec with MockitoSugar {
+
+  private val connector = mock[BindingTariffFilestoreConnector]
+  private val service = new FileService(connector)
+  private implicit val headers: HeaderCarrier = HeaderCarrier()
+
+  "Service" should {
+    val fileUploading = mock[MultipartFormData.FilePart[TemporaryFile]]
+    val fileUploaded = mock[UploadFileResponse]
+
+    "Delegate to connector" in {
+      given(connector.upload(refEq(fileUploading))(any[HeaderCarrier])).willReturn(Future.successful(fileUploaded))
+
+      await(service.uploadFile(fileUploading)) shouldBe fileUploaded
+    }
+  }
+
+}
