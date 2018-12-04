6f9dfbe27975cdb1dc4e62f23363178db258ba37
Stefano Sgorlon <stefano.sgorlon@digital.hmrc.gov.uk>
2018-12-04 17:11:55 +0000

    BAU: reverting to "bootstrap-play-25" version "4.0.0" because version "4.0.1" fails to deploy in QA

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
   val compile = Seq(
     "commons-validator" % "commons-validator"                 % "1.6",
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "bootstrap-play-25"                % "4.1.0",
+    "uk.gov.hmrc"       %% "bootstrap-play-25"                % "4.0.0",
     "uk.gov.hmrc"       %% "govuk-template"                   % "5.26.0-play-25",
     "uk.gov.hmrc"       %% "http-caching-client"              % "8.0.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping"    % "0.2.0",
