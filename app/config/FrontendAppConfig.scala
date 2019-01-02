4465806742ffeb1b5207088bbb83d97710709e20
pllaurado <pol.llaurado@gmail.com>
2019-01-02 14:39:44 +0000

    DIT-594 add external links

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -50,7 +50,7 @@ class FrontendAppConfig @Inject() (override val runModeConfiguration: Configurat
   lazy val bindingTariffFileStoreUrl: String = baseUrl("binding-tariff-filestore")
   lazy val authEnrolment = loadConfig("auth.enrolment")
 
-  lazy val btiUkWebsite = loadConfig("urls.btiUkWebsite")
+  lazy val btiUkWebsite = loadConfig("external-links.btiUkWebsite")
 
   lazy val languageTranslationEnabled = runModeConfiguration.getBoolean("microservice.services.features.welsh-translation").getOrElse(true)
 
