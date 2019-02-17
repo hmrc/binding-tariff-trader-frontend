630df3b4db524fb2ebc46972619935f19cc732f9
Stefano Sgorlon <gokyo@users.noreply.github.com>
2019-02-17 10:01:59 +0000

    Update AppDependencies.scala

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,7 +9,7 @@ object AppDependencies {
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc"       %% "bootstrap-play-25"                % "4.9.0",
     "uk.gov.hmrc"       %% "govuk-template"                   % "5.28.0-play-25",
-    "uk.gov.hmrc"       %% "http-caching-client"              % "8.0.0",
+    "uk.gov.hmrc"       %% "http-caching-client"              % "8.1.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping"    % "0.2.0",
     "uk.gov.hmrc"       %% "play-health"                      % "3.11.0-play-25",
     "uk.gov.hmrc"       %% "play-language"                    % "3.4.0",
