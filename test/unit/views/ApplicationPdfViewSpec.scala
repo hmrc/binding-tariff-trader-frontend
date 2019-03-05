c9a6dae5c155389779f4a35f3a8d1f416c1cce09
Steve Sugden <46934286+SteveSugden@users.noreply.github.com>
2019-03-05 18:11:17 +0000

    More unit tests around the rendered pdf contents & a fix

--- test/unit/views/ApplicationPdfViewSpec.scala
+++ test/unit/views/ApplicationPdfViewSpec.scala
@@ -16,36 +16,78 @@
 
 package views
 
-import models.oCase
+import models.response.FilestoreResponse
+import models.{Case, oCase}
 import org.jsoup.nodes.Document
-import play.twirl.api.Html
 import views.html.pdftemplates.applicationPdf
 
 class ApplicationPdfViewSpec extends ViewSpecBase {
 
-  private val traderCase = oCase.btiCaseExample.copy(application = oCase.btiApplicationExample.copy(agent = None))
-  private val traderAttachments = Seq.empty
-
+  private val traderApplication = oCase.btiApplicationExample.copy(agent = None)
+  private val traderCase = oCase.btiCaseExample.copy(application = traderApplication)
   private val agentCase = oCase.btiCaseExample
-  private val agentAttachments = Seq.empty
 
-  private def createTraderPdfView: () => Html = () => applicationPdf(frontendAppConfig, traderCase, traderAttachments)(fakeRequest, messages)
-  private def createAgentPdfView: () => Html = () => applicationPdf(frontendAppConfig, agentCase, agentAttachments)(fakeRequest, messages)
+  private def createView(c: Case, attachments: Seq[FilestoreResponse]) = applicationPdf(frontendAppConfig, c, attachments)(fakeRequest, messages)
+
 
   "Check Your Answers view" must {
 
     "contain the details for a trader" in {
-      val doc = asDocument(createTraderPdfView())
+      val doc = asDocument(createView(traderCase, Seq.empty))
 
       containsCommonSections(doc)
       assertNotRenderedById(doc, "pdf.application.section.applyingFor.heading")
     }
 
     "contain the details for an agent" in {
-      val doc = asDocument(createAgentPdfView())
+      val doc = asDocument(createView(agentCase, Seq.empty))
 
       containsCommonSections(doc)
       assertRenderedById(doc, "pdf.application.section.applyingFor.heading")
+    }
+
+    "contain the details for re-issued BTI" in {
+      val doc = asDocument(createView(traderCase.copy(application = traderApplication.copy(
+        reissuedBTIReference = Some("REISSUE1234"))), Seq.empty))
+
+      doc.getElementById("application.reissuedBTIReference").text() mustBe "REISSUE1234"
+    }
+
+    "contain the details for confidential information" in {
+      val doc = asDocument(createView(traderCase.copy(application = traderApplication.copy(
+        confidentialInformation = Some("Confidential information"))), Seq.empty))
+
+      doc.getElementById("application.confidentialInformation").text() mustBe "Confidential information"
+    }
+
+    "contain the details for uploaded files" in {
+      asDocument(createView(traderCase, Seq.empty)).getElementById("application.attachments").text() mustBe "None"
+
+      val docFiles = asDocument(createView(traderCase, Seq(
+        FilestoreResponse("id", "pdfFile.pdf", "application/pdf"),
+        FilestoreResponse("id", "image.jpg", "image/jpg"))))
+      docFiles.getElementById("application.attachments").text() mustBe "pdfFile.pdf image.jpg"
+    }
+
+    "contain the details for related BTI case" in {
+      val doc = asDocument(createView(traderCase.copy(application = traderApplication.copy(
+        relatedBTIReference = Some("RELATED1234"))), Seq.empty))
+
+      doc.getElementById("application.relatedBTIReference").text() mustBe "RELATED1234"
+    }
+
+    "contain the details for legal problems" in {
+      val doc = asDocument(createView(traderCase.copy(application = traderApplication.copy(
+        knownLegalProceedings = Some("Legal problems"))), Seq.empty))
+
+      doc.getElementById("application.knownLegalProceedings").text() mustBe "Legal problems"
+    }
+
+    "contain the details for other information" in {
+      val doc = asDocument(createView(traderCase.copy(application = traderApplication.copy(
+        otherInformation = Some("Other information"))), Seq.empty))
+
+      doc.getElementById("application.otherInformation").text() mustBe "Other information"
     }
   }
 
