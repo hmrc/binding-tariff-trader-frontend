1374622e0a936123a4311aae7d1c6d40b2482300
Steve Sugden <46934286+SteveSugden@users.noreply.github.com>
2019-03-08 08:58:07 +0000

    Review - refactor common code

--- app/controllers/PdfDownloadController.scala
+++ app/controllers/PdfDownloadController.scala
@@ -22,6 +22,7 @@ import javax.inject.Inject
 import models.Case
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, AnyContent, Results}
+import play.twirl.api.Html
 import service.{CasesService, FileService, PdfService}
 import uk.gov.hmrc.play.bootstrap.controller.FrontendController
 import views.html.pdftemplates.{applicationPdf, rulingPdf}
@@ -39,23 +40,20 @@ class PdfDownloadController @Inject()(appConfig: FrontendAppConfig,
   def application(reference: String): Action[AnyContent] = identify.async { implicit request =>
     caseService.getCaseForUser(request.eoriNumber, reference) flatMap { c: Case =>
       fileService.getAttachmentMetadata(c) flatMap { attachmentData =>
-        pdfService.generatePdf(applicationPdf(appConfig, c, attachmentData)).map { binaryFile =>
-          Results.Ok(binaryFile.content)
-            .as(binaryFile.contentType)
-            .withHeaders(CONTENT_DISPOSITION -> s"filename=BTIConfirmation$reference.pdf")
-        }
+        generatePdf(applicationPdf(appConfig, c, attachmentData), s"BTIConfirmation$reference.pdf")
       }
     }
   }
 
   def ruling(reference: String): Action[AnyContent] = identify.async { implicit request =>
     caseService.getCaseWithRulingForUser(request.eoriNumber, reference) flatMap { c: Case =>
-      pdfService.generatePdf(rulingPdf(appConfig, c, c.decision.get)).map { binaryFile =>
-        Results.Ok(binaryFile.content)
-          .as(binaryFile.contentType)
-          .withHeaders(CONTENT_DISPOSITION -> s"filename=BTIRuling$reference.pdf")
-      }
+      generatePdf(rulingPdf(appConfig, c, c.decision.get), s"BTIRuling$reference.pdf")
     }
   }
+
+  private def generatePdf(htmlContent: Html, filename: String) = pdfService.generatePdf(htmlContent)
+    .map(pdfFile => {
+      Results.Ok(pdfFile.content).as(pdfFile.contentType).withHeaders(CONTENT_DISPOSITION -> s"filename=$filename")
+    })
 
 }
