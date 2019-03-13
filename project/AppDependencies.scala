7d0a6bc73672d2201816c284a21e21157a9456b2
Stefano Sgorlon <gokyo@users.noreply.github.com>
2019-03-13 23:47:00 +0000

    Update AppDependencies.scala

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -15,7 +15,7 @@ object AppDependencies {
     "uk.gov.hmrc"       %% "play-health"                      % "3.12.0-play-25",
     "uk.gov.hmrc"       %% "play-language"                    % "3.4.0",
     "uk.gov.hmrc"       %% "play-ui"                          % "7.33.0-play-25",
-    "uk.gov.hmrc"       %% "simple-reactivemongo"             % "7.14.0-play-25",
+    "uk.gov.hmrc"       %% "simple-reactivemongo"             % "7.15.0-play-25",
     "uk.gov.hmrc"       %% "play-whitelist-filter"            % "2.0.0"
   )
 
