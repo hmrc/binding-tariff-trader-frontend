553e1f13f11a38f71b372bbb47ba79bd33b8562d
Steve Sugden <46934286+SteveSugden@users.noreply.github.com>
2019-03-27 09:15:27 +0000

    Dit 922 (#95)
    Provide hard maxlength for text input fields to guard against any that do not have a validation length (none at present)
    Remove field length validation from two multi-line text-areas current set at 100.

--- test/unit/forms/CommodityCodeRulingReferenceFormProviderSpec.scala
+++ test/unit/forms/CommodityCodeRulingReferenceFormProviderSpec.scala
@@ -17,13 +17,13 @@
 package forms
 
 import forms.behaviours.StringFieldBehaviours
+import org.scalacheck.Arbitrary.arbitrary
 import play.api.data.FormError
 
 class CommodityCodeRulingReferenceFormProviderSpec extends StringFieldBehaviours {
 
   val requiredKey = "commodityCodeRulingReference.error.required"
   val lengthKey = "commodityCodeRulingReference.error.length"
-  val maxLength = 100
 
   val form = new CommodityCodeRulingReferenceFormProvider()()
 
@@ -34,14 +34,7 @@ class CommodityCodeRulingReferenceFormProviderSpec extends StringFieldBehaviours
     behave like fieldThatBindsValidData(
       form,
       fieldName,
-      stringsWithMaxLength(maxLength)
-    )
-
-    behave like fieldWithMaxLength(
-      form,
-      fieldName,
-      maxLength = maxLength,
-      lengthError = FormError(fieldName, lengthKey, Seq(maxLength))
+      arbitrary[String]
     )
 
     behave like mandatoryField(
