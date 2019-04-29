e2fb7eec78c2d57ff331d16101de72bc50d48bd1
aaroncarroll82 <aaron.carroll@digital.hmrc.gov.uk>
2019-04-29 09:54:30 +0100

    DIT-976 - Adding new page WIP

---
+++ app/controllers/RulingController.scala
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import config.FrontendAppConfig
+import controllers.actions.IdentifierAction
+import javax.inject.Inject
+import models.Case
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent}
+import service.CasesService
+import uk.gov.hmrc.play.bootstrap.controller.FrontendController
+import views.html.ruling_information
+
+import scala.concurrent.Future.successful
+
+class RulingController @Inject()(val appConfig: FrontendAppConfig,
+                                 identify: IdentifierAction,
+                                 service: CasesService,
+                                 val messagesApi: MessagesApi) extends FrontendController with I18nSupport {
+
+  def viewRuling(reference: String): Action[AnyContent] = identify.async { implicit request =>
+
+    request.eoriNumber match {
+      case Some(eori: String) =>
+        service.getCaseForUser(eori,reference) flatMap {
+          c: Case => successful(Ok(ruling_information(appConfig,c)))
+        }
+
+
+      case None => successful(Redirect(routes.BeforeYouStartController.onPageLoad()))
+      //case _ => successful(Redirect(routes.BeforeYouStartController.onPageLoad()))
+    }
+
+
+
+//    request.eoriNumber match {
+//      case Some(eori: String) =>
+//        service.getCases(eori, rulingStatuses, SearchPagination(page), Sort(SortField.DECISION_START_DATE)) flatMap { pagedResult =>
+//          successful(Ok(index(appConfig, CaseDetailTab.RULING, table_rulings(pagedResult))))
+//        }
+//
+//      case None => successful(Redirect(routes.BeforeYouStartController.onPageLoad()))
+//    }
+  }
+}
