3abb4b40c68433b38ec68a7572fe4e816ff68071
Neda Vazharova <neda.vazharova@opencastsoftware.com>
2019-11-11 15:46:54 +0000

    DIT-1196: Upgrade dependencies WIP - fixed layout errors

--- app/handlers/ErrorHandler.scala
+++ app/handlers/ErrorHandler.scala
@@ -21,16 +21,15 @@ import javax.inject.{Inject, Singleton}
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.Request
 import play.twirl.api.Html
-import config.FrontendAppConfig
 import uk.gov.hmrc.play.bootstrap.http.FrontendErrorHandler
 
 @Singleton
 class ErrorHandler @Inject()(
-                              appConfig: FrontendAppConfig,
-                              val messagesApi: MessagesApi
+                              val messagesApi: MessagesApi,
+                              val error_template: views.html.error_template
                             ) extends FrontendErrorHandler with I18nSupport {
 
   override def standardErrorTemplate(pageTitle: String, heading: String, message: String)
                                     (implicit rh: Request[_]): Html =
-    views.html.error_template(pageTitle, heading, message, appConfig)
+    error_template(pageTitle, heading, message)
 }
