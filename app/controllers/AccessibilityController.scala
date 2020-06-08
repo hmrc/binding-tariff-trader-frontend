3979f23dd434f8c780abb00381643685828e6c48
shiromi <shiromi002@yahoo.co.uk>
2020-06-08 10:47:08 +0100

    DIT-1828 Update accessibility statements on Application Service

--- app/controllers/AccessibilityController.scala
+++ app/controllers/AccessibilityController.scala
@@ -21,14 +21,15 @@ import javax.inject.Inject
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import uk.gov.hmrc.play.bootstrap.controller.FrontendController
-import views.html.session_expired
+import views.html.accessibilityView
 
-class SessionExpiredController @Inject()(
-                                          val appConfig: FrontendAppConfig,
-                                          cc: MessagesControllerComponents
-                                        ) extends FrontendController(cc) with I18nSupport {
+
+class AccessibilityController @Inject()(val appConfig: FrontendAppConfig,
+                                        cc: MessagesControllerComponents)
+  extends FrontendController(cc) with I18nSupport  {
+
 
   def onPageLoad: Action[AnyContent] = Action { implicit request =>
-    Ok(session_expired(appConfig))
+    Ok(accessibilityView(appConfig))
   }
 }
