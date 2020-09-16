a635bf80697de68c7545c128a746df51a44c9ce3
Gorkem Unsal <gorkem.unsal@accenture.com>
2020-09-16 16:08:10 +0100

    DIT-2310 fix unit tests for the view

---
+++ test/unit/views/IsSampleHazardousViewSpec.scala
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package unit.views
+
+import controllers.routes
+import forms.IsSampleHazardousFormProvider
+import models.NormalMode
+import play.api.data.Form
+import views.behaviours.YesNoViewBehaviours
+import views.html.isSampleHazardous
+
+class IsSampleHazardousViewSpec extends YesNoViewBehaviours {
+
+  val messageKeyPrefix = "isSampleHazardous"
+
+  val form = new IsSampleHazardousFormProvider()()
+
+  def isSampleHazardousView: isSampleHazardous = injector.instanceOf[isSampleHazardous]
+
+  val fakeGETRequest = fakeGETRequestWithCSRF
+
+  def createView = () =>
+    isSampleHazardousView(frontendAppConfig, form, NormalMode)(fakeGETRequest, messages)
+
+  def createViewUsingForm = (form: Form[_]) =>
+    isSampleHazardousView(frontendAppConfig, form, NormalMode)(fakeGETRequest, messages)
+
+/*
+  override def expectedLegend(messageKeyPrefix: String): String = {
+    messages(s"$messageKeyPrefix.heading")
+  }
+*/
+
+  "IsSampleHazardous view" must {
+
+    behave like normalPage(createView, messageKeyPrefix)()
+
+    behave like pageWithBackLink(createView)
+
+    behave like yesNoPage(createViewUsingForm,
+      messageKeyPrefix,
+      routes.IsSampleHazardousController.onSubmit(NormalMode).url,
+      "isSampleHazardous")
+  }
+}
