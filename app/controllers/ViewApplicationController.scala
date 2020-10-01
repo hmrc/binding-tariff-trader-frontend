18e410823b9648585d5994816443cb76dd528daf
shiromi <shiromi002@yahoo.co.uk>
2020-10-01 14:21:19 +0100

    DIT-2354 initial work for Create application pdf view and layout changes

---
+++ app/controllers/ViewApplicationController.scala
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import config.FrontendAppConfig
+import javax.inject.Inject
+import play.api.i18n.I18nSupport
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import uk.gov.hmrc.play.bootstrap.controller.FrontendController
+import views.html.viewApplication
+
+class ViewApplicationController @Inject()(val appConfig: FrontendAppConfig,
+                                          cc: MessagesControllerComponents)
+  extends FrontendController(cc) with I18nSupport  {
+
+
+  def onPageLoad: Action[AnyContent] = Action { implicit request =>
+    Ok(viewApplication(appConfig))
+  }
+
+}
