8a9c4297edaab43c351b14b61d23f2a9dd03ede0
Gaston Racui <gastonracui@gmail.com>
2023-03-16 11:03:17 +0000

    DDCE-3668 Upgrade binding-tariff-trader-frontend to jdk 11

--- app/forms/mappings/Formatters.scala
+++ app/forms/mappings/Formatters.scala
@@ -96,9 +96,7 @@ trait Formatters {
       override def bind(key: String, data: Map[String, String]): Either[Seq[FormError], Int] =
         baseFormatter
           .bind(key, data)
-          .right
           .map(_.replace(",", ""))
-          .right
           .flatMap {
             case s if s.matches(decimalRegexp) =>
               Left(Seq(FormError(key, wholeNumberKey)))
@@ -121,7 +119,7 @@ trait Formatters {
       private val baseFormatter = stringFormatter(requiredKey)
 
       override def bind(key: String, data: Map[String, String]): Either[Seq[FormError], A] =
-        baseFormatter.bind(key, data).right.flatMap { str =>
+        baseFormatter.bind(key, data).flatMap { str =>
           ev.withName(str).map(Right.apply).getOrElse(Left(Seq(FormError(key, invalidKey))))
         }
 
