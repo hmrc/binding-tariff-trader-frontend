57b4190206aa3f6a030d30b9fcf20101ddd9fe4d
SachaMcCormack <175815565+SachaMcCormack@users.noreply.github.com>
2025-01-16 15:08:02 +0000

    removed commons-io and replace ByteStreamArrayBuffer method

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,20 +2,19 @@ import sbt.*
 
 object AppDependencies {
 
-  private val hmrcMongoPlayVersion = "2.3.0"
-  private val bootstrapPlayVersion = "9.5.0"
+  private val hmrcMongoPlayVersion = "2.4.0"
+  private val bootstrapPlayVersion = "9.6.0"
 
   private lazy val compile: Seq[ModuleID] = Seq(
     "commons-validator"      % "commons-validator"          % "1.9.0",
-    "uk.gov.hmrc"           %% "play-frontend-hmrc-play-30" % "11.7.0",
+    "uk.gov.hmrc"           %% "play-frontend-hmrc-play-30" % "11.10.0",
     "uk.gov.hmrc"           %% "bootstrap-frontend-play-30" % bootstrapPlayVersion,
     "uk.gov.hmrc.mongo"     %% "hmrc-mongo-play-30"         % hmrcMongoPlayVersion,
-    "commons-codec"          % "commons-codec"              % "1.17.1",
+    "commons-codec"          % "commons-codec"              % "1.17.2",
     "uk.gov.hmrc"           %% "play-json-union-formatter"  % "1.22.0",
     "org.typelevel"         %% "cats-core"                  % "2.12.0",
     "org.apache.xmlgraphics" % "fop"                        % "2.10",
-    "net.sf.saxon"           % "Saxon-HE"                   % "12.5",
-    "commons-io"             % "commons-io"                 % "2.18.0"
+    "net.sf.saxon"           % "Saxon-HE"                   % "12.5"
   )
 
   private lazy val test: Seq[ModuleID] = Seq(
