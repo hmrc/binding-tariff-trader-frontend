a394b2154f4650f032b905b5baa683a815e4fb03
suraj-suseelan <186935751+suraj-suseelan@users.noreply.github.com>
2025-03-06 11:13:49 +0000

    DDCE-6439 ATaR - Address vulnerability warnings relating to commons-io

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,15 +3,16 @@ import sbt.*
 object AppDependencies {
 
   private val hmrcMongoPlayVersion = "2.5.0"
-  private val bootstrapPlayVersion = "9.9.0"
+  private val bootstrapPlayVersion = "9.11.0"
 
   private lazy val compile: Seq[ModuleID] = Seq(
     "commons-validator"      % "commons-validator"          % "1.9.0",
-    "uk.gov.hmrc"           %% "play-frontend-hmrc-play-30" % "11.11.0",
+    "uk.gov.hmrc"           %% "play-frontend-hmrc-play-30" % "11.12.0",
     "uk.gov.hmrc"           %% "bootstrap-frontend-play-30" % bootstrapPlayVersion,
     "uk.gov.hmrc.mongo"     %% "hmrc-mongo-play-30"         % hmrcMongoPlayVersion,
     "commons-codec"          % "commons-codec"              % "1.18.0",
     "org.typelevel"         %% "cats-core"                  % "2.13.0",
+    "commons-io"             % "commons-io"                 % "2.18.0",
     "org.apache.xmlgraphics" % "fop"                        % "2.10",
     "net.sf.saxon"           % "Saxon-HE"                   % "12.5"
   )
