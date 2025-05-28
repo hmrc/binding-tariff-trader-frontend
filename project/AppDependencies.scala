e99738cea9268b8bfb355d916ac2874e99573538
gastonracui <gastonracui@gmail.com>
2025-05-28 12:15:32 +0100

    DDCE-6614 Standardise repository.yaml (#435)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,18 +3,18 @@ import sbt.*
 object AppDependencies {
 
   private val hmrcMongoPlayVersion = "2.6.0"
-  private val bootstrapPlayVersion = "9.11.0"
+  private val bootstrapPlayVersion = "9.12.0"
 
   private lazy val compile: Seq[ModuleID] = Seq(
     "commons-validator"      % "commons-validator"          % "1.9.0",
-    "uk.gov.hmrc"           %% "play-frontend-hmrc-play-30" % "12.0.0",
+    "uk.gov.hmrc"           %% "play-frontend-hmrc-play-30" % "12.1.0",
     "uk.gov.hmrc"           %% "bootstrap-frontend-play-30" % bootstrapPlayVersion,
     "uk.gov.hmrc.mongo"     %% "hmrc-mongo-play-30"         % hmrcMongoPlayVersion,
     "commons-codec"          % "commons-codec"              % "1.18.0",
     "org.typelevel"         %% "cats-core"                  % "2.13.0",
     "commons-io"             % "commons-io"                 % "2.19.0",
     "org.apache.xmlgraphics" % "fop"                        % "2.10",
-    "net.sf.saxon"           % "Saxon-HE"                   % "12.6"
+    "net.sf.saxon"           % "Saxon-HE"                   % "12.7"
   )
 
   private lazy val test: Seq[ModuleID] = Seq(
