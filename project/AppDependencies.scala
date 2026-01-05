3a686017a5029ca6c6907d7ee45d86e9a593f30a
suraj-suseelan <186935751+suraj-suseelan@users.noreply.github.com>
2026-01-05 16:45:19 +0000

    DDCE-7511: dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,11 +10,11 @@ object AppDependencies {
     "uk.gov.hmrc"           %% "play-frontend-hmrc-play-30" % "12.25.0",
     "uk.gov.hmrc"           %% "bootstrap-frontend-play-30" % bootstrapPlayVersion,
     "uk.gov.hmrc.mongo"     %% "hmrc-mongo-play-30"         % hmrcMongoPlayVersion,
-    "commons-codec"          % "commons-codec"              % "1.20.0",
+    "commons-codec"          % "commons-codec"              % "1.19.0",
     "org.typelevel"         %% "cats-core"                  % "2.13.0",
-    "commons-io"             % "commons-io"                 % "2.21.0",
+    "commons-io"             % "commons-io"                 % "2.20.0",
     "org.apache.xmlgraphics" % "fop"                        % "2.10",
-    "net.sf.saxon"           % "Saxon-HE"                   % "12.9"
+    "net.sf.saxon"           % "Saxon-HE"                   % "12.8"
   )
 
   private lazy val test: Seq[ModuleID] = Seq(
