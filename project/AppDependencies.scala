d882c0d1ba14e9d9539d67d3bc663c30f7f2975f
sacha.okus@gmail.com <104149202+SachaOkus@users.noreply.github.com>
2026-01-28 09:18:18 +0000

    Depencdency Update

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt.*
 
 object AppDependencies {
 
-  private val hmrcMongoPlayVersion = "2.11.0"
+  private val hmrcMongoPlayVersion = "2.12.0"
   private val bootstrapPlayVersion = "10.5.0"
 
   private lazy val compile: Seq[ModuleID] = Seq(
